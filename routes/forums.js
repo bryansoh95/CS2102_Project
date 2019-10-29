bada0d931bc82e9a1f4415ab14672d03914c48ec
Jun Le <tayjunle@gmail.com>
2019-10-30 02:09:01 +0800

    add more crud queries and posts search

--- routes/forums.js
+++ routes/forums.js
@@ -2,14 +2,37 @@ const express = require('express')
 const pool = require('../pool')
 const router = express.Router()
 
+const GET_ALL_THREADS_FOR_COURSE = `
+SELECT * 
+FROM Threads
+WHERE module_code = $1
+AND category = $2
+`
+
 const NEW_THREAD_ENTRY = `
 INSERT INTO Threads
 VALUES ($1, $2, $3, $4, NOW())
 `
 
+const GET_NEXT_POST_ID = `
+SELECT MAX(COALESCE(post_id, 0))
+FROM Posts 
+WHERE module_code = $1 
+AND category = $2 
+AND thread_title = $3
+`
+
 const NEW_POST_ENTRY = `
 INSERT INTO Posts
-VALUES ($1, $2, $3, $4, $5, NOW(), $7)
+VALUES ($1, $2, $3, $4, $5, NOW(), $6)
+`
+
+const GET_ALL_POSTS_FOR_THREAD = `
+SELECT * 
+FROM Posts
+WHERE module_code = $1
+AND category = $2
+AND thread_title = $3
 `
 
 const AMEND_POST_ENTRY = `
@@ -21,6 +44,35 @@ AND category = $4
 AND thread_title = $5
 AND post_id = $6
 `
+
+const SEARCH_FOR_FORUM_POSTS = `
+SELECT * FROM Posts
+WHERE module_code = $1
+AND category = $2
+AND post_content LIKE '%' || $3 || '%'
+`
+
+const SEARCH_FOR_THREAD_POSTS = `
+SELECT * FROM Posts
+WHERE module_code = $1
+AND category = $2
+AND thread_title = $3
+AND post_content LIKE '%' || $4 || '%'
+`
+
+router.get('/course/:module_code/forum/:category/thread', (req, res, next) => {
+    const data = {
+        module_code: req.body.module_code,
+        category: req.body.category
+    }
+    pool.query(GET_ALL_THREADS_FOR_COURSE, [data.module_code, data.category], (err, dbRes) => {
+        if (err) {
+            res.send('error!')
+        } else {
+            res.send(dbRes.rows)
+        }
+    })
+})
 
 router.post('/course/:module_code/forum/:category/thread/new', (req, res, next) => {
     const data = {
@@ -34,6 +86,45 @@ router.post('/course/:module_code/forum/:category/thread/new', (req, res, next) 
             res.send('error!')
         } else {
             res.send('insert success')
+        }
+    })
+})
+
+router.get('/course/:module_code/forum/:category/thread/:thread_title/post', (req, res, next) => {
+    const data = {
+        module_code: req.body.module_code,
+        category: req.body.category,
+        thread_title: req.body.thread_title
+    }
+    pool.query(GET_ALL_POSTS_FOR_THREAD, [data.module_code, data.category, data.thread_title], (err, dbRes) => {
+        if (err) {
+            res.send('error!')
+        } else {
+            res.send(dbRes.rows)
+        }
+    })
+})
+
+router.post('/course/:module_code/forum/:category/thread/:thread_title/posts/new', (req, res, next) => {
+    const data = {
+        module_code: req.body.module_code,
+        category: req.body.category,
+        thread_title: req.body.thread_title,
+        post_content: req.body.post_content,
+        uname: req.body.uname
+    }
+    pool.query(GET_NEXT_POST_ID, [data.module_code, data.category, data.thread_title], (err, dbRes) => {
+        if (err) {
+            res.send('error!')
+        } else {
+            const post_id = dbRes.rows[0].max + 1
+            pool.query(NEW_POST_ENTRY, [data.module_code, data.category, data.thread_title, data.post_content, post_id, data.uname], (err, dbRes) => {
+                if (err) {
+                    res.send('error!')
+                } else {
+                    res.send('new post entry success')
+                }
+            })
         }
     })
 })
@@ -53,6 +144,37 @@ router.put('/course/:module_code/forum/:category/thread/:thread_title/posts/:pos
             res.send('error!')
         } else {
             res.send('update success')
+        }
+    })
+})
+
+router.get('/course/:module_code/forum/:category/search', (req, res, next) => {
+    const data = {
+        module_code: req.body.module_code,
+        category: req.body.category,
+        search_input: req.body.search_input
+    }
+    pool.query(SEARCH_FOR_FORUM_POSTS, [data.module_code, data.category, data.search_input], (err, dbRes) => {
+        if (err) {
+            res.send('error!')
+        } else {
+            res.send(dbRes.rows)
+        }
+    })
+})
+
+router.get('/course/:module_code/forum/:category/thread/:thread_title/search', (req, res, next) => {
+    const data = {
+        module_code: req.body.module_code,
+        category: req.body.category,
+        thread_title: req.body.thread_title,
+        search_input: req.body.search_input
+    }
+    pool.query(SEARCH_FOR_THREAD_POSTS, [data.module_code, data.category, data.thread_title, data.search_input], (err, dbRes) => {
+        if (err) {
+            res.send('error!')
+        } else {
+            res.send(dbRes.rows)
         }
     })
 })
