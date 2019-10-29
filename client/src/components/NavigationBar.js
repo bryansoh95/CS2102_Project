c19263eaf3a5f8c616b97b39c9e9adea87a8ccad
Jefferson <jefflowzh@live.com>
2019-10-29 13:22:20 +0800

    Added some front end boilerplate

---
+++ client/src/components/NavigationBar.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
+import { SSL_OP_TLS_ROLLBACK_BUG } from "constants";
+
+class NavigationBar extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar style={{ background: "rgb(2, 122, 197)" }} light expand="md">
+          <NavbarBrand style={{ color: "white" }} href="/">
+            Luminus2.0
+          </NavbarBrand>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink style={{ color: "white" }} href="/components/">
+                NavItem1
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                style={{ color: "white" }}
+                href="https://github.com/reactstrap/reactstrap"
+              >
+                GitHub
+              </NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle style={{ color: "white" }} nav caret>
+                Logout
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>Option 1</DropdownItem>
+                <DropdownItem>Option 2</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Reset</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default NavigationBar;
---
+++ client/src/pages/Landing.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Landing extends Component {
+  render() {
+    return <div>Landing/login page</div>;
+  }
+}
+
+export default Landing;
