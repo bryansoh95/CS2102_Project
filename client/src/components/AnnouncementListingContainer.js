c82f62a6aad1b6c2bf257963ed449d464d1ae276
Bryan Soh <bryansoh95@gmail.com>
2019-11-02 17:04:33 +0800

    Populate announcements on landing

--- client/src/components/AnnouncementListingContainer.js
+++ client/src/components/AnnouncementListingContainer.js
@@ -2,29 +2,38 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { ListGroup, ListGroupItem } from "reactstrap";
 import axios from "axios";
+import { connect } from "react-redux";
 
 class AnnouncementListingContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      studentModuleAnnouncements: [
-        "cs2102 announcement lorem ipsum",
-        "cs1101 announcement",
-        "is1103 announcement",
-        "cs2030 announcement",
-        "is2102 announcement"
-      ]
+      studentModuleAnnouncements: []
     };
+  }
+  componentDidMount() {
+    axios
+      .post("/course/student", {
+        username: this.props.user.username
+      })
+      .then(res => {
+        this.setState({ studentModuleAnnouncements: res.data });
+      })
+      .catch(err => console.log(err));
   }
   render() {
     return (
       <ListGroup>
-        {this.state.studentModuleAnnouncements.map(module => (
-          <ListGroupItem>{module}</ListGroupItem>
+        {this.state.studentModuleAnnouncements.map(announcement => (
+          <ListGroupItem>{announcement.content}</ListGroupItem>
         ))}
       </ListGroup>
     );
   }
 }
 
-export default AnnouncementListingContainer;
+const mapStateToProps = state => ({
+  user: state.user
+});
+
+export default connect(mapStateToProps)(AnnouncementListingContainer);
