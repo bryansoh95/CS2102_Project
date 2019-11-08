2c8f6e6ef2d16fa8a98c9acf76a764c230e4b300
Bryan Soh <bryansoh95@gmail.com>
2019-11-08 21:24:38 +0800

    Added update function for announcements

--- client/src/components/FormA.js
+++ client/src/components/FormA.js
@@ -1,89 +1,121 @@
-import React, { useState } from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Input, Label, Form, FormGroup } from 'reactstrap';
-import axios from 'axios';
+import React, { useState } from "react";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Input,
+  Label,
+  Form,
+  FormGroup
+} from "reactstrap";
+import axios from "axios";
 import { connect } from "react-redux";
 
-const FormA = (props) => {
-    const {
-        firstPostRoute,
-        secondPostRoute,
-        data,
-        buttonLabel,
-        firstField,
-        secondField,
-        formHeader,
-        action
-    } = props;
+const FormA = props => {
+  const {
+    firstPostRoute,
+    secondPostRoute,
+    data,
+    buttonLabel,
+    firstField,
+    secondField,
+    formHeader,
+    action
+  } = props;
 
-    const [modal, setModal] = useState(false);
-    const [unmountOnClose, setUnmountOnClose] = useState(true);
+  const [modal, setModal] = useState(false);
+  const [unmountOnClose, setUnmountOnClose] = useState(true);
 
-    const toggle = () => setModal(!modal);
+  const toggle = () => setModal(!modal);
 
-    var firstInput
-    var secondInput
+  var firstInput;
+  var secondInput;
 
-    const onChangeOne = e => {
-       firstInput = e.target.value
-    }
+  const onChangeOne = e => {
+    firstInput = e.target.value;
+  };
 
-    const onChangeTwo = e => {
-        secondInput = e.target.value
-     }
+  const onChangeTwo = e => {
+    secondInput = e.target.value;
+  };
 
-    const handleSubmit = () => {
-        data['uname'] = props.user.username
-        data['puname'] = props.user.username
-        data['thread_title'] = firstInput
-        data['title'] = firstInput
-        data['suname'] = firstInput
-        data['content'] = secondInput
-        data['max_mark'] = secondInput
-        data['score'] = secondInput
-        data['tutorial_group'] = secondInput
-        axios.post(firstPostRoute, data)
-        .then(res => {
-            if (secondPostRoute) {
-                data['post_content'] = secondInput
-                axios.post(secondPostRoute, data)
-                .then(res => {
-                    alert('new thread and post started!')
-                })
-                .catch(err => {
-                    console.log(err)
-                })
-            }
-            window.location.reload()
-        })
-        .catch(err => {
-            console.log(err)
-        })
-    }
+  const handleSubmit = () => {
+    data["uname"] = props.user.username;
+    data["puname"] = props.user.username;
+    data["thread_title"] = firstInput;
+    data["title"] = firstInput;
+    data["suname"] = firstInput;
+    data["new_title"] = firstInput;
+    data["content"] = secondInput;
+    data["new_content"] = secondInput;
+    data["max_mark"] = secondInput;
+    data["score"] = secondInput;
+    data["tutorial_group"] = secondInput;
+    axios
+      .post(firstPostRoute, data)
+      .then(res => {
+        if (secondPostRoute) {
+          data["post_content"] = secondInput;
+          axios
+            .post(secondPostRoute, data)
+            .then(res => {
+              alert("new thread and post started!");
+            })
+            .catch(err => {
+              console.log(err);
+            });
+        }
+        window.location.reload();
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
-    return (
-        <div>
-            <Form inline onSubmit={(e) => e.preventDefault()}>
-                <Button color="info" onClick={toggle}>{buttonLabel}</Button>
-            </Form>
-            <Modal isOpen={modal} toggle={toggle} unmountOnClose={unmountOnClose}>
-                <ModalHeader toggle={toggle}>{formHeader}</ModalHeader>
-                <ModalBody>
-                    <h5 className='mb-4'>{firstField}</h5>
-                    <Input id='firstInput' type="textarea" placeholder={"Enter the " + firstField} onChange={onChangeOne} rows={2} />
-                </ModalBody>
-                <ModalBody>
-                    <h5 className='mb-4'>{secondField}</h5>
-                    <Input id='secondInput' type="textarea" placeholder={"Enter the " + secondField} onChange={onChangeTwo} rows={5} />
-                </ModalBody>
-                <ModalFooter>
-                    <Button color="primary" onClick={handleSubmit}>{action}</Button>{' '}
-                    <Button color="secondary" onClick={toggle}>Cancel</Button>
-                </ModalFooter>
-            </Modal>
-        </div>
-    );
-}
+  return (
+    <div>
+      <Form inline onSubmit={e => e.preventDefault()}>
+        <Button color="info" onClick={toggle}>
+          {buttonLabel}
+        </Button>
+      </Form>
+      <Modal isOpen={modal} toggle={toggle} unmountOnClose={unmountOnClose}>
+        <ModalHeader toggle={toggle}>{formHeader}</ModalHeader>
+        <ModalBody>
+          <h5 className="mb-4">{firstField}</h5>
+          <Input
+            id="firstInput"
+            type="textarea"
+            placeholder={"Enter the " + firstField}
+            onChange={onChangeOne}
+            rows={2}
+          />
+        </ModalBody>
+        <ModalBody>
+          <h5 className="mb-4">{secondField}</h5>
+          <Input
+            id="secondInput"
+            type="textarea"
+            placeholder={"Enter the " + secondField}
+            onChange={onChangeTwo}
+            rows={5}
+          />
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={handleSubmit}>
+            {action}
+          </Button>{" "}
+          <Button color="secondary" onClick={toggle}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+};
 
-const mapStateToProps = state => ({user: state.user})
+const mapStateToProps = state => ({ user: state.user });
 
 export default connect(mapStateToProps)(FormA);
--- client/src/pages/Announcements.js
+++ client/src/pages/Announcements.js
@@ -10,7 +10,7 @@ import {
   ListGroupItemText
 } from "reactstrap";
 import axios from "axios";
-import FormA from '../components/FormA'
+import FormA from "../components/FormA";
 import { connect } from "react-redux";
 
 class Announcements extends Component {
@@ -20,7 +20,7 @@ class Announcements extends Component {
   }
 
   componentDidMount() {
-    console.log(this.props.module_code);
+    console.log(this);
     axios
       .post("/course/announcements", {
         module_code: this.props.module_code
@@ -42,28 +42,63 @@ class Announcements extends Component {
           <Col>
             <Row className="mt-5">
               <Col sm={{ size: 5, order: 1 }}>
-                <h1 >{this.props.module_code} Announcements</h1>
+                <h1>{this.props.module_code} Announcements</h1>
               </Col>
-              <Col style={{
-                display:
-                  this.props.user.username.substring(0, 1) === "A"
-                    ? "block"
-                    : "none"
-              }} sm={{ size: 4, order: 1 }} className='mt-2'>
-                <FormA firstPostRoute='/course/announcements/new' buttonLabel='Add new Announcement' formHeader='Add new Announcement' firstField='Announcement Title' secondField='Announcement Details' action='Post' data={{ 'module_code': this.props.module_code }} />
+              <Col
+                style={{
+                  display:
+                    this.props.user.username.substring(0, 1) === "A"
+                      ? "block"
+                      : "none"
+                }}
+                sm={{ size: 4, order: 1 }}
+                className="mt-2"
+              >
+                <FormA
+                  firstPostRoute="/course/announcements/new"
+                  buttonLabel="Add new Announcement"
+                  formHeader="Add new Announcement"
+                  firstField="Announcement Title"
+                  secondField="Announcement Details"
+                  action="Post"
+                  data={{ module_code: this.props.module_code }}
+                />
               </Col>
             </Row>
             <ListGroup>
               {this.state.moduleAnnouncements.map(announcement => (
                 <ListGroupItem>
-                  <ListGroupItemHeading>
-                    {announcement.title}
-                    <hr />
-                  </ListGroupItemHeading>
-                  <ListGroupItemText>{announcement.content}</ListGroupItemText>
-                  <ListGroupItemText>
-                    {announcement.timestamp} | {announcement.name}
-                  </ListGroupItemText>
+                  <Row>
+                    <Col>
+                      <ListGroupItemHeading>
+                        {announcement.title}
+                        <hr />
+                      </ListGroupItemHeading>
+                      <ListGroupItemText>
+                        {announcement.content}
+                      </ListGroupItemText>
+                      <ListGroupItemText>
+                        {announcement.timestamp} | {announcement.name}
+                      </ListGroupItemText>
+                    </Col>
+                    <Col></Col>
+                    <Col></Col>
+                    <Col></Col>
+                    <Col>
+                      <FormA
+                        firstPostRoute="/course/announcements/edit"
+                        buttonLabel="EDIT"
+                        formHeader="Edit Announcement"
+                        firstField="Announcement Title"
+                        secondField="Announcement Details"
+                        action="Post"
+                        data={{
+                          module_code: this.props.module_code,
+                          old_title: announcement.title
+                        }}
+                      />
+                    </Col>
+                  </Row>
                 </ListGroupItem>
               ))}
             </ListGroup>
@@ -76,6 +111,6 @@ class Announcements extends Component {
 
 const mapStateToProps = state => ({
   user: state.user
-})
+});
 
 export default connect(mapStateToProps)(Announcements);
