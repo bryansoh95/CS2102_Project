dff6f7960b4168fea35d01a20bb33a1459701da4
Jun Le <tayjunle@gmail.com>
2019-11-08 19:38:17 +0800

    add delete assessment

--- client/src/pages/Assessment.js
+++ client/src/pages/Assessment.js
@@ -1,63 +1,86 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import SideNav from "../components/SideNav";
-import { Row, Col, ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from "reactstrap";
+import { Row, Col, ListGroup, ListGroupItem, ListGroupItemHeading, Button } from "reactstrap";
 import { connect } from "react-redux";
 import axios from "axios"
 import FormA from '../components/FormA'
 
 class Assessment extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      moduleAssessments: []
+    constructor(props) {
+        super(props)
+        this.state = {
+            moduleAssessments: []
+        }
     }
-  }
-  componentDidMount() {
-    axios.post('/course/assessment', {
-      module_code: this.props.module_code,
-    })
-      .then(res => {
-        this.setState({ moduleAssessments: res.data })
-      })
-      .catch(err => {
-        console.log(err)
-      })
-  }
-  render() {
-    return (
-      <div>
-        <Row>
-          <Col xs="3">
-            <SideNav module_code={this.props.module_code} />
-          </Col>
-          <Col>
-            <Row className="mt-5">
-              <Col sm={{ size: 4, order: 1 }}>
-                <h1>{this.props.module_code} Assessment</h1>
-              </Col>
-              <Col className='mt-2' sm={{ size: 3, order: 2 }}>
-                <FormA firstPostRoute='/course/assessment/add' data={{ "module_code": this.props.module_code }} buttonLabel='Create Assessment' formHeader='Create Assessment' firstField='Assessment Title' secondField='Max Marks' action='Create' />
-              </Col>
-            </Row>
-            <ListGroup className="mr-5">
-              {this.state.moduleAssessments.map(assessment => (
-                <Link to={'/modules/' + this.props.module_code + '/assessment/' + assessment.title}>
-                  <ListGroupItem>
-                    <ListGroupItemHeading>{assessment.title}</ListGroupItemHeading>
-                  </ListGroupItem>
-                </Link>
-              ))}
-            </ListGroup>
-          </Col>
-        </Row>
-      </div>
-    );
-  }
+    componentDidMount() {
+        axios.post('/course/assessment', {
+            module_code: this.props.module_code,
+        })
+            .then(res => {
+                this.setState({ moduleAssessments: res.data })
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    handleClick = index => {
+        axios.post('/course/assessment/delete', {
+            module_code: this.props.module_code,
+            title: this.state.moduleAssessments[index].title,
+            puname: this.props.user.username
+        })
+            .then(res => {
+                alert('delete success!')
+                window.location.reload()
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                <Row>
+                    <Col xs="3">
+                        <SideNav module_code={this.props.module_code} />
+                    </Col>
+                    <Col>
+                        <Row className="mt-5">
+                            <Col sm={{ size: 4, order: 1 }}>
+                                <h1>{this.props.module_code} Assessment</h1>
+                            </Col>
+                            <Col className='mt-2' sm={{ size: 3, order: 2 }}>
+                                <FormA firstPostRoute='/course/assessment/add' data={{ "module_code": this.props.module_code }} buttonLabel='Create Assessment' formHeader='Create Assessment' firstField='Assessment Title' secondField='Max Marks' action='Create' />
+                            </Col>
+                        </Row>
+                        <ListGroup className="mr-5">
+                            {this.state.moduleAssessments.map((assessment, index) => (
+                                <ListGroupItem>
+                                    <Row>
+                                        <Col sm={{ size: 8, order: 1 }}>
+                                            <Link to={'/modules/' + this.props.module_code + '/assessment/' + assessment.title}>
+                                                <ListGroupItemHeading>{assessment.title}</ListGroupItemHeading>
+                                            </Link>
+                                        </Col>
+                                        <Col sm={{ size: 2, order: 1 }}>
+                                            <Button color="danger" onClick={() => this.handleClick(index)}>Delete</Button>{' '}
+                                        </Col>
+                                    </Row>
+                                </ListGroupItem>
+                            ))}
+                        </ListGroup>
+                    </Col>
+                </Row>
+            </div>
+        );
+    }
 }
 
 const mapStateToProps = state => ({
-  user: state.user
+    user: state.user
 });
 
 export default connect(mapStateToProps)(Assessment);
